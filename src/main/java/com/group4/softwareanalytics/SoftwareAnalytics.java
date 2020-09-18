14be8f227ccb4b76e74691c2c0a29cf33d7c043f
GREGORY-PC\Gregory <wullimann.gregory@gmail.com>
2020-09-18 16:19:46 +0200

    Initial setup

---
+++ src/main/java/com/group4/softwareanalytics/SoftwareAnalytics.java
@@ -0,0 +1,14 @@
+
+package com.group4.softwareanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareAnalytics {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareAnalytics.class, args);
+	}
+}
+
