3b9eeaa8ca00fd3676c743fbe78126eda90fefd2
GREGORY-PC\Gregory <wullimann.gregory@gmail.com>
2020-09-18 16:27:09 +0200

    Added frontend

---
+++ frontendVue/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="container mt-3">
+		<div class="row">
+			<div class="col">
+				<h1>Software Analytics - Group 4</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<div class="input-group">
+					<input type="text" placeholder="Analyze a repository" aria-label="Repository search" class="form-control"> 
+					<div class="input-group-append">
+						<button type="submit" class="btn btn-secondary">
+							<i class="material-icons text-white align-middle">search</i>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+
+	};
+</script>
