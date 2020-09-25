1628487b5e4fd86e7423ee0389e2a2451a75e9e0
Simone <masies@usi.ch>
2020-09-25 12:54:35 +0200

    clean dependency for repoRepository

--- backend/src/main/java/com/group4/softwareanalytics/RepoRepository.java
+++ backend/src/main/java/com/group4/softwareanalytics/RepoRepository.java
@@ -1,7 +1,6 @@
 package com.group4.softwareanalytics;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
